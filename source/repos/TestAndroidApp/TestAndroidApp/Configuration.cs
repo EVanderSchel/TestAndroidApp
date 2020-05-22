1bde4ab54f5eaafda40e736ed51be8db02b0b65b
ethan.vanderschel@gmail.com <ethan.vanderschel@gmail.com>
2020-05-21 22:15:15 -0500

    first commit

---
+++ source/repos/TestAndroidApp/TestAndroidApp/Configuration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace TestAndroidApp
+{
+    public static class Configuration
+    {
+        public const string ClientId = "0oacwk0josOoqvb934x6";
+
+        public const string LoginRedirectUri = "com.okta.dev-994458:/callback";
+
+        public const string OrgUrl = "https://dev-994458-admin.okta.com/";
+    }
+}
---
+++ source/repos/TestAndroidApp/TestAndroidApp/Models/AuthorizationResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace TestAndroidApp.Models
+{
+    class AuthorizationResult
+    {
+        public string AccessToken { get; set; }
+
+        public string IdentityToken { get; set; }
+
+        public bool IsAuthorized { get; set; }
+
+        public string RefreshToken { get; set; }
+
+        public string Scope { get; set; }
+    }
+}
